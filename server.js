fc814e6f8596a5ab318f0b7181cf77a6ba0d5994
alanam79 <halo2382001@gmail.com>
2022-06-07 17:34:08 -0400

    getting api get and post established

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const res = require('express/lib/response');
-const PORT = process.env.PORT || 3001;
-const app = express();
 const { animals } = require('./data/animals');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-
-
-
-
-
-
-
-
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 
 
@@ -61,8 +59,37 @@ function filterByQuery(query, animalsArray) {
     return result;
   }
 
+  function createNewAnimal(body, animalsArray) {
+    // console.log(body);
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    )
+    // return finished code to post route for response
+    return animal;
+  }
 
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
 
+// user requesting information from server - start
+// this is a GET request which means the user is requesting data
+// from the server
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
@@ -79,9 +106,28 @@ app.get('/api/animals', (req, res) => {
       res.send(404);
     }
   });
+  // user requesting information from server - end
+
+        // user can add information to our server - start
+        // this is a POST request which is different from the above GET, POST represent
+        // the action of a client requesting the server accept data rather than vice versa for GET
+  app.post('/api/animals', (req, res) => {
+        // req.body is where our incoming content will be
+        // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+    // console.log(req.body);
+
+    if (!validateAnimal(req.body)) {
+      res.status(400).send("The animal is not properly formated.");
+    } else {
+        // add animal to json file and animals array in this function
+      const animal = createNewAnimal(req.body, animals);
+
+      res.json(animal);
+    }
+  });  // user can add information to our server - end
 
 // server
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
-
